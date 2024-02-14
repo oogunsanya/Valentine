fe86d0bc6fcfdc7103139c541930b4669da06544
Oluwaseun Ogunsanya <oogunsanya@hawk.iit.edu>
2024-02-13 20:04:51 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 document.getElementById('yesBtn').addEventListener('click', function() {
   document.getElementById('response').innerText = "I knew you'd say yes 🤣 We going out tomorrow 🎉🍾 I love you! 💜💜💜";
+  // Create confetti
+  createConfetti();
 });
+
+function createConfetti() {
+  const confetti = document.createElement('div');
+  confetti.classList.add('confetti');
+  document.body.appendChild(confetti);
+
+  confetti.style.left = `${Math.random() * 100}%`;
+  confetti.style.animationDuration = `${Math.random() * 2 + 1}s`;
+
+  setTimeout(() => {
+    confetti.remove();
+  }, 3000);
+}
 
 document.getElementById('noBtn').addEventListener('click', function() {
   document.getElementById('response').innerText = "Oh no, that's not an option. try again! 🤣";
