25e2ca05ae40d5305c6e81f90a610067ff8334bb
Oluwaseun Ogunsanya <oogunsanya@hawk.iit.edu>
2024-02-13 19:55:00 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('yesBtn').addEventListener('click', function() {
-  document.getElementById('response').innerText = "That's wonderful! I love you! 💖";
+  document.getElementById('response').innerText = "I knew you'd say yes 🤣 We going out tomorrow 🎉🍾 I love you! 💜💜💜";
 });
 
 document.getElementById('noBtn').addEventListener('click', function() {
