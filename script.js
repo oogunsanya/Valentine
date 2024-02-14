0081809bb234422b428db4448206736b86cc74e2
Oluwaseun Ogunsanya <oogunsanya@hawk.iit.edu>
2024-02-13 19:22:13 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('yesBtn').addEventListener('click', function() {
+  document.getElementById('response').innerText = "That's wonderful! I love you! 💖";
+});
+
+document.getElementById('noBtn').addEventListener('click', function() {
+  document.getElementById('response').innerText = "Oh, I'm sorry to hear that. Maybe next time! 😔";
+});
